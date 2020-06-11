d84b127836bdef6b8955d1a1878c6a6c2311e1cc
hhk <790182710@qq.com>
2020-06-11 10:39:47 +0800

    add 修改hosts跨域
    Change-Id: I59803d96218328be2fdd4a939c367aff0e763856

--- learn/test/ReadJson/index.js
+++ learn/test/ReadJson/index.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     var url = "/demo.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
-    // var url = "http://hanhuankang.com/demo.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+    // var url = "http://hanhuankang.com:8000/demo.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+    //  8000需要跟本地端口号一致 hosts文件配置 127.0.0.1 指向 hanhuankang.com
     var request = new XMLHttpRequest();
     request.open("get", url);/*设置请求方法与路径*/
     request.send(null);/*不发送数据到服务器*/
